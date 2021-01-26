f4f62cff0fc6a5e68148047ce30c397b1689054b
unknown <kartikkhanna93@gmail.com>
2021-01-26 10:56:26 +0530

    readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Contact-Us
+1.change your mail id and password in the settings.py file
+2.open your gmail go to manage account.
+3.go to security
+4.turn on mail from less secure app.
--- contactus/settings.py
+++ contactus/settings.py
@@ -128,5 +128,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER='kartikkhanna93@gmail.com'
-EMAIL_HOST_PASSWORD='Kartik*123'
+EMAIL_HOST_USER='kartikkhanna93@gmail.com' #enter your mail here 
+EMAIL_HOST_PASSWORD='password' #enter your password for the mail here
